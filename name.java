9a880a086f6569e7dce246c836fbaf103ccef4cd
yugandhargithub <97966120+yugandhargithub@users.noreply.github.com>
2022-01-28 23:59:18 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class name {
+
+	public static void main(String[] args) {
+		 String s;
+		    double p;
+		   Scanner sc=new Scanner(System.in);
+		    System.out.println("Enter your name");
+		    s=sc.nextLine();
+		    System.out.println("Enter your percentage");
+		    p=sc.nextDouble();
+		     System.out.println("Your name is = "+s);
+		      System.out.println("Your percentage is="+p);
+
+	}
+
+}
